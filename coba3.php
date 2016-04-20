5481386245e8d87abfac375a8ae179662526b6bc
nina3195 <ninamelina31@gmail.com>
2016-04-20 14:31:50 +0700

    Added files via upload

---
+++ coba3.php
@@ -0,0 +1,37 @@
+<html>  
+	<head> 
+		<title> 
+			Background Image
+		</title>
+		<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1"> 
+    <link href="style.css" rel="stylesheet" type="text/css"> 
+	</head> 
+	<body background="vector_spider_web_design_background_graphics_544178.jpg">  
+        <marquee onmouseover="this.stop()" onmouseout="this.start()" truespeed="true" scrollamount="1" scrolldelay="40" direction="left">  
+        <strong>
+			<div id="login">WELCOME TO OUR WEB CRAWLER</div>
+		</strong>  
+    </marquee>  
+
+		<p style="font:bold 50px Georgia ;color:yellow" align="center">Selamat Menikmati Web Crawler ^^</p> 
+		<hr color="ff0000" size="4"> 
+	
+          <div align="center" class="tengah">
+			<p align="center">
+			<font face="verdana" size="4" color="white">
+			Input dengan url / keyword :  </font>
+			</p>
+
+    <form action="" method="post" name="postform">
+		
+			<font face="verdana" size="2" color="white" >URL</br>
+			<input type="text" name="url"/>
+			</br>Keyword</br>
+			<input type="text" name="keyword" /></br></br>
+			<input type="submit" name="Crawl" value="Crawl"/></br></br></br>
+			<input type="text" name="output" value=" (output skrip)" /> 
+		</font>
+	</form>
+
+</div>
+</html>
